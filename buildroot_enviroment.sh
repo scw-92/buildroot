d4b38af2045f0066b9423b511e7940d7c4eac21f
scw-92 <33088619+scw-92@users.noreply.github.com>
2017-12-05 13:22:35 +0800

    一键搭建buildroot的工作环境
    一键搭建buildroot的工作环境

---
+++ buildroot_enviroment.sh
@@ -0,0 +1,11 @@
+ #!/bin/sh     
+ sudo apt-get update -y
+ sudo sudo apt-get install git -y
+ sudo wget  https://buildroot.org/downloads/buildroot-2017.02.3.tar.gz
+ sudo tar xvf buildroot-2017.02.3.tar.gz
+ sudo apt-get install texinfo -y
+ sudo apt-get install libncurses5-dev -y 
+ sudo add-apt-repository ppa:webupd8team/java -y
+ sudo apt-get update
+ sudo apt-get install oracle-java8-installer -y
+ sudo apt-get install oracle-java8-set-default -y
