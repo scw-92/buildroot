f2c644e3796d85ae9328caab23d1f63ecfcc134f
scw-92 <33088619+scw-92@users.noreply.github.com>
2017-12-28 14:32:37 +0800

    Update buildroot_enviroment.sh

--- buildroot_enviroment.sh
+++ buildroot_enviroment.sh
@@ -1,8 +1,7 @@
  #!/bin/sh     
  sudo apt-get update -y
- sudo sudo apt-get install git -y
- sudo wget  https://buildroot.org/downloads/buildroot-2017.02.3.tar.gz
- sudo tar xvf buildroot-2017.02.3.tar.gz
+ wget  https://buildroot.org/downloads/buildroot-2017.02.3.tar.gz
+ tar xvf buildroot-2017.02.3.tar.gz
  sudo apt-get install texinfo -y
  sudo apt-get install libncurses5-dev -y 
  sudo add-apt-repository ppa:webupd8team/java -y
